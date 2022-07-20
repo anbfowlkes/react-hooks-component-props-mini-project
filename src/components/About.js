618b04cb070c7392ff12e77d985b955026fcc6fc
Aaron Fowlkes <afowlkes21@gmail.com>
2022-07-20 17:04:46 -0400

    first commit

---
+++ src/components/About.js
@@ -0,0 +1,23 @@
+
+
+
+const About = ( {image} ) => {
+
+
+    return (
+        <aside>
+            <img src={image} alt={'blog logo'}/>
+            <p></p>
+        </aside>
+    )
+}
+
+
+
+
+
+
+
+
+
+export default About
---
+++ src/components/Header.js
@@ -0,0 +1,21 @@
+
+
+
+const Header = ( {blogName} ) => {
+
+    return (
+        <header>
+            <h1>{blogName}</h1>
+        </header>
+    )
+}
+
+
+
+
+
+
+
+
+
+export default Header
